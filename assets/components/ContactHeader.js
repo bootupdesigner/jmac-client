35dcb01d87d6578c8561fe50d0fcff53a2615766
bootupdesigner <bootup.itinfo@gmail.com>
2024-10-11 01:23:18 -0400

    fixed contactheader to include linking

--- assets/components/ContactHeader.js
+++ assets/components/ContactHeader.js
@@ -1,44 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Linking } from 'react-native'
 import React from 'react'
 import Feather from '@expo/vector-icons/Feather';
 
 const ContactHeader = () => {
-  return (
-    <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        padding: 10,
-        backgroundColor: 'black',
-    }}>
-
+    return (
         <View style={{
             flexDirection: 'row',
+            justifyContent: 'space-around',
             alignItems: 'center',
-            width:'50%',
+            padding: 10,
+            backgroundColor: 'black',
         }}>
-            <Feather name="phone-call" size={16} color="white" />
-            <Text
-                style={{
 
-                    paddingHorizontal: 5, color: 'white', fontWeight: 'bold', fontSize: 16
-                }}
-                onPress={() => Linking.openURL("tel:+13057937944")}>Call</Text>
-        </View>
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                width: '50%',
+            }}>
+                <Feather name="phone-call" size={16} color="white" />
+                <Text
+                    style={{
 
-        <View style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-        }}>
-            <Feather name="mail" size={16} color="white" />
-            <Text
-                style={{
-                    paddingHorizontal: 5, color: 'white', fontWeight: 'bold', fontSize: 16
-                }}
-                onPress={() => Linking.openURL("mailto:jmaccleaningservices@gmail.com")}>Email</Text>
+                        paddingHorizontal: 5, color: 'white', fontWeight: 'bold', fontSize: 16
+                    }}
+                    onPress={() => Linking.openURL("tel:+13057937944")}>Call</Text>
+            </View>
+
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+            }}>
+                <Feather name="mail" size={16} color="white" />
+                <Text
+                    style={{
+                        paddingHorizontal: 5, color: 'white', fontWeight: 'bold', fontSize: 16
+                    }}
+                    onPress={() => Linking.openURL("mailto:jmaccleaningservices@gmail.com")}>Email</Text>
+            </View>
         </View>
-    </View>
-  )
+    )
 }
 
 export default ContactHeader
